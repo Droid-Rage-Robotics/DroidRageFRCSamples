dd89b15e00ad440a45ffec51b528017ee3cab7e5
FTC10862Nebula <droidrage.nebula@gmail.com>
2023-05-01 16:38:02 -0500

    Advantage Kit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "DroidRageFRCSamples";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "abb545526d8091f4def4726afb981a814469f1f8";
+  public static final String GIT_DATE = "2023-04-27 20:25:20 GMT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2023-04-30 00:51:56 GMT";
+  public static final long BUILD_UNIX_TIME = 1682815916526L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
