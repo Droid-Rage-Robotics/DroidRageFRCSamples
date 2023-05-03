3d5e3145f257f5ecd3e720c019166c54ed1eb6a1
FTC10862Nebula <droidrage.nebula@gmail.com>
2023-05-03 11:21:17 -0500

    Added RobotContainer and Fixed Robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,11 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.server.PathPlannerServer;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,58 +17,95 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
  * directory.
  */
 public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
-  private final XboxController m_controller = new XboxController(0);
-  private final Timer m_timer = new Timer();
+  private final RobotContainer robotContainer = new RobotContainer();
+  private Command autonomousCommand;
+  
 
   /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightDrive.setInverted(true);
-  }
-
-  /** This function is run once each time the robot enters autonomous mode. */
-  @Override
-  public void autonomousInit() {
-    m_timer.reset();
-    m_timer.start();
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-    // Drive for 2 seconds
-    if (m_timer.get() < 2.0) {
-      // Drive forwards half speed, make sure to turn input squaring off
-      m_robotDrive.arcadeDrive(0.5, 0.0, false);
-    } else {
-      m_robotDrive.stopMotor(); // stop robot
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        // autonomous chooser on the dashboard.
+        PathPlannerServer.startServer(5811); // Use to see the Path of the robot on PathPlanner
+        // robotContainer = new RobotContainer();
     }
-  }
+    
+    /**
+     * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+     * that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+        // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+        // commands, running already-scheduled commands, removing finished or interrupted commands,
+        // and running subsystem periodic() methods.  This must be called from the robot's periodic
+        // block in order for anything in the Command-based framework to work.
+        CommandScheduler.getInstance().run();
+    }
 
-  /** This function is called once each time the robot enters teleoperated mode. */
-  @Override
-  public void teleopInit() {}
+    @Override
+    public void disabledInit() {
 
-  /** This function is called periodically during teleoperated mode. */
-  @Override
-  public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
-  }
+    }
+    
+    @Override
+    public void disabledPeriodic() {
+        // if(RobotController.getBatteryVoltage()<11.5){
+        //     light.setAllColor(light.batteryBlue);
+        // } else{
+        //     light.flashingColors(light.yellow, light.blue);
+        // }
+        
+        //TODO: Add lights to have the robot tell us any errors with can, etc.
+        // light.rainbow();
+        // light.orangeAndBlue();
+        // light.switchLeds();
+        // light.chaseLED( 1);
+    }
 
-  /** This function is called once each time the robot enters test mode. */
-  @Override
-  public void testInit() {}
+    @Override
+    public void autonomousInit() {
+        CommandScheduler.getInstance().cancelAll();
+        autonomousCommand = robotContainer.getAutonomousCommand();
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+        if (autonomousCommand != null) {
+            autonomousCommand.schedule();
+        }
+    }
+
+    @Override
+    public void autonomousPeriodic() {}
+    
+    @Override
+    public void teleopInit() {
+        CommandScheduler.getInstance().cancelAll();
+        robotContainer.configureTeleOpBindings();
+        // robotContainer.configureTeleOpDriverOnlyBindings();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        robotContainer.teleopPeriodic();
+    }
+    
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+        robotContainer.configureTestBindings();
+    }
+
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void simulationInit() {}
+
+    @Override
+    public void simulationPeriodic() {}
 }
